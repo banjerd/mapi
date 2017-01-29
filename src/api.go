2ada47ed8fd76760fd894d90c53994727104b5ee
banjerdp <banjerdp@wisesoft.co.th>
2017-01-29 15:21:52 +0700

    no message

--- src/api.go
+++ src/api.go
@@ -8,7 +8,7 @@ import (
 func main() {
   e := echo.New()
   e.GET("/",func(c echo.Context) error {
-    return c.String(http.StatusOK,"Hello World")
+    return c.JSON(http.StatusOK,"Hello World")
   })
   e.Logger.Fatal(e.Start(":1323"))
 }
